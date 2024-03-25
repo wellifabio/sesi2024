92cd957ef717cc29aadbcd59320b048fdfbff52e
wellifabio <wellifabio@hotmail.com>
2024-03-25 09:44:10 -0300

    aula07

---
+++ 2em/02-sop/aula07/ajusta.bat
@@ -0,0 +1,9 @@
+move ./*.html ./paginas/
+move ./*.css ./css/
+move ./*.js ./js/
+move ./*.sql ./"banco de dados"/
+move ./*.png ./assets/
+move ./*.txt ./relatorios/
+rm bd
+del bancodedados.bd
+tree /F
---
+++ 2em/02-sop/aula07/cria.bat
@@ -0,0 +1,15 @@
+md assets
+md "banco de dados"
+md css
+md js
+md bd
+md paginas
+md relatorios
+type NUL > bancodedados.bd
+type NUL > bancodedados.sql
+type NUL > home.html
+type NUL > imagem01.png
+type NUL > imagem02.png
+type NUL > relatorio.txt
+type NUL > script.js
+type NUL > style.css
