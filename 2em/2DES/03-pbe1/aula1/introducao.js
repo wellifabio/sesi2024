a7c88b21495400ed68f8a462835575093dada34d
wellifabio <wellifabio@hotmail.com>
2024-07-26 11:30:21 -0300

    introducao

---
+++ 2em/2DES/03-pbe1/aula1/introducao.js
@@ -0,0 +1,17 @@
+/*
+Introdução a Java Script 
+Uma linguagem interpretada, ou seja, não é necessário compilar o código para executá-lo.
+Uma linguagem sincrona, ou seja, executa várias instruções ao mesmo tempo.
+Uma linguagem de tipagem fraca, ou seja, não é necessário declarar o tipo da variável.
+Uma linguagem funcional, ou seja, é possível passar funções como parâmetros.
+Uma linguagem orientada a objetos, ou seja, é possível criar objetos.
+*/
+console.log("Alo mundo!");
+alert("Alo mundo!");
+x = prompt("Digite um numero");
+y = prompt("Digite outro numero");
+console.log(x + y);
+z = parseInt(x) + parseInt(y);
+console.log(z);
+
+//Comandos básicos
---
+++ 2em/2DES/03-pbe1/aula1/triangulo.js
@@ -0,0 +1,21 @@
+/*
+Crie um programa que leia os três lados de um triângulo
+e classifique, se é um triângulo equilátero, isósceles ou escaleno.
+*/
+
+a = parseFloat(prompt("Digite o lado A"));
+b = parseFloat(prompt("Digite o lado B"));
+c = parseFloat(prompt("Digite o lado C"));
+
+// Verifica se é um triângulo
+if(a + b > c && a + c > b && b + c > a){
+    if(a == b && b == c){
+        console.log("Triângulo equilátero");
+    }else if(a != b && b != c && c != a){
+        console.log("Triângulo escaleno");
+    }else{
+        console.log("Triângulo isósceles");
+    }
+}else{
+    console.log("Não é um triângulo");
+}
