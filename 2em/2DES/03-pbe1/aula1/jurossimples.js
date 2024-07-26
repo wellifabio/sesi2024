3987d8f228d3fe6f4905eba7b2d1d6627744099b
wellifabio <wellifabio@hotmail.com>
2024-07-26 13:22:25 -0300

    juros

---
+++ 2em/2DES/03-pbe1/aula1/jurossimples.js
@@ -0,0 +1,11 @@
+/*
+Juros Simples - Crie um programa que leia um valor inicial,
+uma taxa de juros e um período em meses e calcule o montante final
+com juros simples.
+*/
+
+/*
+Juros Compostos - Crie um programa que leia um valor inicial,
+uma taxa de juros e um período em meses e calcule o montante final
+com juros compostos.
+*/
